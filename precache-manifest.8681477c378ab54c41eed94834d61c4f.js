f2823c7d938f4685c2af085e46bca99151cc0511
dthrasher90 <dthrasher90@GMAIL.COM>
2020-08-15 10:35:39 -0700

    Updates

---
+++ precache-manifest.8681477c378ab54c41eed94834d61c4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad5be97833c895de1068c9f90c5465a",
+    "url": "/testp3/index.html"
+  },
+  {
+    "revision": "d22719911c19621ce0d6",
+    "url": "/testp3/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/testp3/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testp3/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d22719911c19621ce0d6",
+    "url": "/testp3/static/js/main.507000a4.chunk.js"
+  },
+  {
+    "revision": "ded545084cf86901b433",
+    "url": "/testp3/static/js/runtime-main.3aecb14c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testp3/static/media/logo.5d5d9eef.svg"
+  }
+]);
